d8e7710bbffdb6453d8afbbc97c79a6faffde948
qimi <15217710611@163.com>
2024-10-11 23:54:52 +0800

    Fix edge to edge

--- app/src/main/java/com/qimi/app/qplayer/MainActivity.kt
+++ app/src/main/java/com/qimi/app/qplayer/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             val appState: AppState = rememberAppState()
