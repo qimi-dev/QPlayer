f1f42899752811bed380dbd06ce4b1b3e96188aa
qimi <15217710611@163.com>
2024-10-13 20:30:37 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.qimi.app.qplayer"
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
